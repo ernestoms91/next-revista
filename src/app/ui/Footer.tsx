5ccde80565ca75b9a93403f27a45ab6de1a5fdf0
ernestoms91 <ernestoms91@gmail.com>
2023-11-23 01:39:49 -0500

    added footer

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -4,30 +4,71 @@ import { otros, publicidad, secciones } from "@/app/lib/footer-data";
 
 const Footer = () => {
   return (
-    <footer className="bg-gris-oscuro py-5 px-5">
-      <div className="flex items-center justify-center ">
-        <Image src="/capa1.svg" alt="Logo" width={250} height={40} priority />
+    <footer className="bg-gris-oscuro py-5 px-5 ">
+      <div className="md:px-20 md:py-10 md:grid md:grid-cols-3 md:grid-rows-2">
+      {/* */}
+      <div className="">
+        <div className="flex items-center justify-center md:justify-start ">
+          <Image
+            className="md:hidden"
+            src="/capa1.svg"
+            alt="Logo"
+            width={250}
+            height={40}
+            priority
+          />
+          <Image
+            className="hidden md:block"
+            src="/logotipo.svg"
+            alt="Logo"
+            width={250}
+            height={40}
+            priority
+          />
+        </div>
+        <div className="flex py-5 justify-between md:justify-normal md:gap-10">
+          <Image
+            src="/facebook.svg"
+            alt="Logo"
+            width={50}
+            height={20}
+            priority
+          />
+          <Image
+            src="/twitter.svg"
+            alt="Logo"
+            width={50}
+            height={20}
+            priority
+          />
+          <Image src="/in.svg" alt="Logo" width={50} height={20} priority />
+          <Image
+            src="/youtube.svg"
+            alt="Logo"
+            width={50}
+            height={20}
+            priority
+          />
+        </div>
       </div>
-      <div className="flex py-5 justify-between">
-        <Image src="/facebook.svg" alt="Logo" width={50} height={20} priority />
-        <Image src="/twitter.svg" alt="Logo" width={50} height={20} priority />
-        <Image src="/in.svg" alt="Logo" width={50} height={20} priority />
-        <Image src="/youtube.svg" alt="Logo" width={50} height={20} priority />
-      </div>
-      <div className="md:hidden">
-        <h1 className=" text-xl  text-white ">Secciones</h1>
-        <div className="grid grid-cols-3  justify-center md:grid-cols-none">
+      <div className="md:col-span-2 md:row-span-2 md:flex">
+        <h1 className=" text-xl  text-white md:hidden ">Secciones</h1>
+        <div className="grid grid-cols-3  justify-center md:grid-cols-none  md:mx-20">
+          <h1 className=" text-xl  text-white hidden md:block">Secciones</h1>
           {secciones.map((s, i) => {
             return (
               <Link
-                className={`text-gray-300 text-lg my-2
+                className={`text-gray-300 text-lg my-2 
                 ${
-                (i + 1) % 3 === 0 ? "text-right" :
-                i + 1  === 2 ? "text-center" :
-                i + 1  === 5 ? "text-center" :
-                "text-left"} 
-                ${i === 0 && 'text-left' ? i === 2 && 'text-right' : 'text-center'} 
-                `}
+                  (i + 1) % 3 === 0
+                    ? "text-right"
+                    : i + 1 === 2
+                    ? "text-center"
+                    : i + 1 === 5
+                    ? "text-center"
+                    : "text-left"
+                } 
+                md:text-left md:my-2`}
                 key={s.name}
                 href={s.href}
               >
@@ -36,8 +77,8 @@ const Footer = () => {
             );
           })}
         </div>
-        <div className="flex my-5 gap-3">
-          <div>
+        <div className="flex my-5 gap-3 md:my-0">
+          <div className="md:order-last md:ml-10">
             <h1 className=" text-xl  text-white">Publicidad</h1>
             {publicidad.map((p) => {
               return (
@@ -51,7 +92,7 @@ const Footer = () => {
               );
             })}
           </div>
-          <div>
+          <div className="md:ml-20">
             <h1 className=" text-xl  text-white">Otros</h1>
             {otros.map((o, i) => {
               return (
@@ -69,26 +110,27 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
       <div className="flex gap-x-4">
         <Image
           src="/editorialpueblo.svg"
-          className="mb-1"
+          className="mb-1 md:mb-0"
           alt="Logo"
           width={100}
           height={20}
           priority
         />
 
-        <h1 className=" flex mx-4 items-end  font-semibold text-lg  text-gray-300">
+        <h1 className=" flex mx-4 items-end  font-semibold text-lg  text-gray-300 md:font-normal md:mb-7">
           Una revista de
           <br />
           Editorial Pueblo y Educación
         </h1>
       </div>
-
-        <p className="text-xs my-4 text-gray-300 md:text-right md:text-sm">
-          © 2023 Editorial Pueblo y Educación.Todos los derechos reservados
-        </p>
+</div>
+      <p className="text-xs my-4 text-gray-300 md:text-right md:text-sm md:col-span-3 md:my-0">
+        © 2023 Editorial Pueblo y Educación.Todos los derechos reservados
+      </p>
     </footer>
   );
 };
